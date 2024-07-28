9b035fa70341bb4f56b1814e70e58fc4896c3504
Sosuke Suzuki <aosukeke@gmail.com>
2024-07-28 17:31:57 +0900

    Implement writer methods

---
+++ src/cell.cpp
@@ -0,0 +1,30 @@
+#include "cell.h"
+
+namespace PGC {
+
+void Cell::writeBarrier(Cell* parent, Cell* oldChild, Cell* newChild)
+{
+}
+
+void Cell::writeToPtr1(Cell* parent, Cell* newChild)
+{
+    auto oldChild = parent->m_ptr1;
+    writeBarrier(parent, oldChild, newChild);
+    parent->m_ptr1 = newChild;
+}
+
+void Cell::writeToPtr2(Cell* parent, Cell* newChild)
+{
+    auto oldChild = parent->m_ptr2;
+    writeBarrier(parent, oldChild, newChild);
+    parent->m_ptr2 = newChild;
+}
+
+void Cell::writeToPtr3(Cell* parent, Cell* newChild)
+{
+    auto oldChild = parent->m_ptr3;
+    writeBarrier(parent, oldChild, newChild);
+    parent->m_ptr3 = newChild;
+}
+
+} /* PGC */
--- src/cell.h
+++ src/cell.h
@@ -13,11 +13,19 @@ class Cell {
 public:
     Cell() = delete;
     ~Cell() = delete;
+
     CellStatus m_cellStatus;
     size_t m_refs;
+
     Cell* m_ptr1;
     Cell* m_ptr2;
     Cell* m_ptr3;
+
+    static void writeBarrier(Cell* parent, Cell* oldChild, Cell* newChild);
+
+    static void writeToPtr1(Cell* parent, Cell* newChild);
+    static void writeToPtr2(Cell* parent, Cell* newChild);
+    static void writeToPtr3(Cell* parent, Cell* newChild);
 };
 
 } /* PGC */
